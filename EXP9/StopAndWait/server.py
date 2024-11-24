7737a4948236436bcc4b2029d03609f1715f64dd
astumehta <143540555+astumehta@users.noreply.github.com>
2024-11-24 17:30:10 +0530

    changes made

---
+++ EXP9/StopAndWait/server.py
@@ -0,0 +1,27 @@
+import socket
+import time
+HOST='0.0.0.0'
+PORT=5001
+server_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_socket.bind((HOST,PORT))
+server_socket.listen()
+print(f"the server is listening on {HOST}:{PORT}")
+conn,addr=server_socket.accept()
+print(f"connected by {addr}")
+def generate_ack(conn):
+    while True:
+        data=conn.recv(1024)
+        if not data:
+            break
+        received_data=data.decode()
+        time.sleep(2)
+        print(f"the data which is received is: {received_data}")
+
+        if received_data=='end':
+            print("the client ended the communicaton")
+            break
+        ack=f"sending the acknowledgement of: {received_data}"
+        conn.sendall(ack.encode())
+    conn.close()
+generate_ack(conn)
+server_socket.close()
--- Exp10/Leakybucket/client.py
+++ Exp10/Leakybucket/client.py
@@ -1,19 +1,18 @@
 import socket
 import time
 import random
-
+#current rate,feedback,packetsize
 class Leaky:
     current_rate = 10
-
     def adjust_rate(self, feedback):
         if feedback == "congested":
             self.current_rate = max(1, self.current_rate - 1)
         elif feedback == "underutilized":
             self.current_rate += 1
 
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 leaky = Leaky()
-
 while True:
     packet_size = random.randint(1, 20)
     message = f"Packet of size {packet_size}"
